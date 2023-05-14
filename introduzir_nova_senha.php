5f599dd6c41bc67d2048257e0e9efe5ba18238f7
Domingos Nkula Pedro <domingosnkulajw@gmail.com>
2023-05-14 07:50:02 +0100

    Atualizando...

--- introduzir_nova_senha.php
+++ introduzir_nova_senha.php
@@ -14,18 +14,26 @@ endif;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+     <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+
     <title>Recuperar senha</title>
 </head>
 <body>
 
-        <form action="./functions/alterarSenha.php" method="POST">
-            <div class="input-box">
-                <label>Nova senha</label>
-                <input type="text" placeholder="Introduza uma nova senha" name="nova_senha">
-            </div>
+        <?php include_once __DIR__ . './components/html_mensagens_flash.php'; ?>
 
-            <button type="submit">Alterar senha</button>
-        </form>
+        <section class="form-container">
+
+            <form action="./functions/alterarSenha.php" method="POST">
+                <h3>Nova senha</h3>
+                <input type="text" name="nova_senha" required placeholder="Crie uma nova senha" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
+                <input type="submit" value="Alterar" name="submit" class="btn">
+            </form>
+
+        </section>
     
 </body>
 </html>
--- recuperar_senha.php
+++ recuperar_senha.php
@@ -43,8 +43,8 @@ endif;
    
     <!-- font awesome cdn link  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-
     <link rel="stylesheet" href="./css/style.css">
+    
     <title>Recuperar senha</title>
 </head>
 <body>
