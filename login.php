0c65eca74cdf158780e072046ea831948e88064d
Domingos Nkula Pedro <domingosnkulajw@gmail.com>
2023-05-14 18:27:46 +0100

    Atualizando...

--- login.php
+++ login.php
@@ -21,14 +21,34 @@ if(isset($_POST['submit'])){
    $select_user->execute([$email, $pass]);
    $row = $select_user->fetch(PDO::FETCH_ASSOC);
 
-   if($select_user->rowCount() > 0){
+   if($select_user->rowCount() > 0):
       $_SESSION['user_id'] = $row['id'];
       $_SESSION["mensagens"][] = 'Logado com sucesso!';
       header('location:home.php');
       return;
-   }else{
+   
+   else:
+      $stmt = $conn->prepare("SELECT acesso, id FROM usuario WHERE email = ?");
+      $stmt->execute([$email]);
+
+      if($stmt->rowCount() > 0):
+         $user = $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
+         $user_id = $user["id"];
+         $count_acess = intval($user["acesso"]) - 1;
+
+         if($user["acesso"] > 0):
+            $stmt = $conn->prepare("UPDATE usuario SET acesso = ? WHERE id = ?");
+            $stmt->execute([$count_acess, $user_id]);
+
+            $_SESSION["mensagens"][] = "Resta(m) $count_acess tentativa(s)";
+         
+         else:
+            $_SESSION["mensagens"][] = "Infelizmente você foi bloqueado por errar a senha três vezes seguidas!";
+         endif;
+      endif;
+
       $_SESSION["mensagens"][] = 'O nome de usuario ou senha está incorrecta!';
-   }
+   endif;
 
 }
 
