19eb6bfd27cfaf55d27abee96f0df3896b78ae69
Domingos Nkula Pedro <domingosnkulajw@gmail.com>
2023-06-06 23:03:51 +0100

    Atualizando...

--- others/index.php
+++ others/index.php
@@ -34,8 +34,6 @@ if(isset($_POST['submit'])){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Iniciar sessão</title>
-      <p>predefinido nome de usuario = <span>admin</span> & senha = <span>111</span></p>
-
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
